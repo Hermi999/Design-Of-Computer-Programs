2d972040e9a19f07bde0216169d75b0286c95edd
Hermi999 <hermann.wagner01@gmx.at>
2017-03-11 14:13:59 +0100

    finished missionaries and cannibals proglem

--- missionariesAndCannibals.py
+++ missionariesAndCannibals.py
@@ -10,20 +10,61 @@ def csuccessors(state):
       where 'MM->' means two missionaries travel to the right side.
     """
     M1, C1, B1, M2, C2, B2 = state
-    if M1 < C1 or M2 < C2:
+    if C1 > M1 > 0 or C2 > M2 > 0:
         return {}       # canibals are more one one side. They eat the missionaries
-    else:
-        if B1 > 0:  # boat is on the left side
-            return dict(((M1-m, C1-c, 0, M2+m, C2+c, 1), ('M'*m + 'C'*c + "->"))
-            for m in range(min(M1,2)+1)
-            for c in range(min(C1,(2-m))+1)
-            if m != 0 or c != 0)
-        else:
-            return dict(((M1+m, C1+c, 1, M2-m, C2-c, 0), ("<-" + 'M'*m + 'C'*c))
-            for m in range(min(M2,2)+1)
-            for c in range(min(C2,(2-m))+1)
-            if m != 0 or c != 0)
+    
+    # dict containing 5 entries where the keys are 5 vectors and the values are the actions
+    deltas = {(2, 0, 1,     -2,  0, -1): "MM",
+              (0, 2, 1,      0, -2, -1): "CC",
+              (1, 1, 1,     -1, -1, -1): "MC",
+              (1, 0, 1,     -1,  0, -1): "M",
+              (0, 1, 1,      0, -1, -1): "C",
+             }
+    items = []
+    if B1 > 0:
+        items += [(sub(state, delta), a + '->')
+                    for delta, a in deltas.items()]
+    if B2 > 0:
+        items += [(add(state, delta), '<-' + a)
+                    for delta, a in deltas.items()]
+    return dict(items)
 
+def add(X, Y):
+    "add two vectors X and Y."
+    return tuple(x+y for x,y in zip(X, Y))
+
+def sub(X, Y):
+    "subtract vectors Y from X."
+    return tuple(x-y for x,y in zip(X, Y))
+
+
+def mc_problem(start=(3, 3, 1, 0, 0, 0), goal=None):
+    """ Solve the missionaries and cannibals problem
+    State is 6 ints: (M1, C1, B1, M2, C2, B2) on the start (1) and other (2) sides.
+    Find a path that goes from the initial state to the goal state (which, if 
+    not spcified, is the state with no people or boats on the start side)."""
+    
+    if goal is None:
+        goal = (0, 0, 0) + start[:3]    # standard (0,0,0,3,3,1)
+    if start == goal:
+        return [start]
+    explored = set()        # set of states we have visited
+    frontier = [[start]]    # ordered list of paths we have blazed
+    
+    while frontier:
+        path = frontier.pop(0)  # remove the first element from the frontier for adding the next action and state
+        s = path[-1]
+        for(state,action) in csuccessors(s).items():
+            if state not in explored:
+                explored.add(state)     # track every explored state
+                path2 = path + [action, state]  # add the next state with the action to this path
+                if state == goal:       # we reached the goal
+                    return path2
+                else:
+                    frontier.append(path2)  # add this updated path to the frontier
+    return Fail
+
+Fail = []
 
 def test():
     assert csuccessors((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->', 
