265579ed4038de6087a51ba5ddb7b1936319c2fb
Hermi999 <hermann.wagner01@gmx.at>
2017-03-19 19:00:51 +0100

    implemented optimal pig game strategy (= propability of winning)

---
+++ optimal_pig.py
@@ -0,0 +1,117 @@
+# States are represented as a tuple of (p, me, you, pending) where
+# p:       an int, 0 or 1, indicating which player's turn it is.
+# me:      an int, the player-to-move's current score
+# you:     an int, the other player's current score.
+# pending: an int, the number of points accumulated on current turn, not yet scored
+
+other = {1:0, 0:1}  # mapping from player to other player
+goal = 40
+
+from functools import update_wrapper
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+@decorator
+def memo(f):
+    """Decorator that caches the return value for each call to f(args).
+    Then when called again with same args, we can just look it up."""
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args)
+            return result
+        except TypeError:
+            # some element of args can't be a dict key
+            return f(args)
+    return _f
+
+def hold(state):
+    """Apply the hold action to a state to yield a new state:
+    Reap the 'pending' points and it becomes the other player's turn."""
+    p, me, you, pending = state
+    return (other[p], you, me+pending, 0)
+
+def roll(state, d):
+    """Apply the roll action to a state (and a die roll d) to yield a new state:
+    If d is 1, get 1 point (losing any accumulated 'pending' points),
+    and it is the other player's turn. If d > 1, add d to 'pending' points."""
+    p, me, you, pending = state
+    if d > 1:
+        return (p, me, you, pending+d)      # accumulate die roll in pending
+    else:
+        return (other[p], you, me+1, 0)     # pig out
+
+
+def Q_pig(state, action, Pwin):
+    "The expected value of choosing action in state"
+    if action == "hold":
+        return 1 - Pwin(hold(state))
+    if action == "roll":
+        return (1 - Pwin(roll(state, 1))
+                + sum(Pwin(roll(state, d)) for d in (2,3,4,5,6))) / 6
+                
+    raise ValueError
+
+def pig_actions(state):
+    "The legal actions from a state."
+    _, _, _, pending = state
+    return ["roll", "hold"] if pending else ["roll"]
+
+@memo
+def Pwin(state):
+    """ The utility of a state; here just the propapility that an optimal player
+    whose turn it is to move can win from the current state. """
+    # Assumes opponent also plays with optimal strategy
+    (p, me, you, pending) = state
+    if me + pending >= goal:
+        return 1
+    elif you >= goal:
+        return 0
+    else:
+        return max(Q_pig(state, action, Pwin) for action in pig_actions(state))
+    
+def best_action(state, actions, Q, U):
+    "Return the optimal action for a state, given U."
+    def EU(action): return Q(state, action, U)
+    return max(actions(state), key=EU)
+
+
+def max_wins(state):
+    "The optimal pig strategy chooses an action with the highest win probability."
+    return best_action(state, pig_actions, Q_pig, Pwin)
+
+
+def test():
+    assert(max_wins((1, 5, 34, 4)))   == "roll"
+    assert(max_wins((1, 18, 27, 8)))  == "roll"
+    assert(max_wins((0, 23, 8, 8)))   == "roll"
+    assert(max_wins((0, 31, 22, 9)))  == "hold"
+    assert(max_wins((1, 11, 13, 21))) == "roll"
+    assert(max_wins((1, 33, 16, 6)))  == "roll"
+    assert(max_wins((1, 12, 17, 27))) == "roll"
+    assert(max_wins((1, 9, 32, 5)))   == "roll"
+    assert(max_wins((0, 28, 27, 5)))  == "roll"
+    assert(max_wins((1, 7, 26, 34)))  == "hold"
+    assert(max_wins((1, 20, 29, 17))) == "roll"
+    assert(max_wins((0, 34, 23, 7)))  == "hold"
+    assert(max_wins((0, 30, 23, 11))) == "hold"
+    assert(max_wins((0, 22, 36, 6)))  == "roll"
+    assert(max_wins((0, 21, 38, 12))) == "roll"
+    assert(max_wins((0, 1, 13, 21)))  == "roll"
+    assert(max_wins((0, 11, 25, 14))) == "roll"
+    assert(max_wins((0, 22, 4, 7)))   == "roll"
+    assert(max_wins((1, 28, 3, 2)))   == "roll"
+    assert(max_wins((0, 11, 0, 24)))  == "roll"
+    return 'tests pass'
+
+print(test())
+
+
+
